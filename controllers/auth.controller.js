d65ddf3e7e5c6a37c0c99e1a05f6411bd1338447
Teboho02 <moloiteboho0209@gmail.com>
2025-08-19 11:53:24 +0200

    remove safe site to allow authentication to work

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -131,13 +131,13 @@ export const login = async (req, res) => {
     res.cookie('accessToken', accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'Strict',
+      sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'Strict',
+      sameSite: 'None',
       maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
     });
 
